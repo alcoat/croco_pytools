78f8ff507154b2855e50af5eeeca4643f708243b
marc.foletto <marc.foletto@gmail.com>
2023-02-03 16:21:59 +0100

    refacto: create map_tools

--- Modules/graphicUI_tools/main_window.py
+++ Modules/graphicUI_tools/main_window.py
@@ -26,6 +26,8 @@ from Modules.croco_class import CROCO
 
 from Modules.graphicUI_tools.outputs_ui import OutputsUi
 import make_grid_param as param
+
+from Modules.map_tools.map_tools import plot_grid, plot_topo
         
 #################################
 #### FOR FIGURE #################
@@ -506,102 +508,13 @@ class MainWindow(HasTraits):
         self.results_string = (string + "\n" + self.results_string)[0:1000]
 
     def grid_show(self,smooth=None):
-        """
-        Plots an image on the canvas in a thread safe way.
-        """
-        def outline(lon, lat):
-            '''
-            Return lon, lat of perimeter around the grid
-            '''
-            def func(var):
-                return np.hstack([var[:, 0], var[-1, 1:-1],
-                                  var[::-1, -1], var[0, ::-1][1:]])
-            return func(lon), func(lat)
-
-        x_rho, y_rho = self.outputs.lon_rho,self.outputs.lat_rho
-        x_psi, y_psi = self.outputs.lon_psi,self.outputs.lat_psi
-
-        ox_psi, oy_psi  = outline(self.outputs.lon_psi[1:-1, 1:-1], self.outputs.lat_psi[1:-1, 1:-1])
-
-        if min(x_rho.ravel())<180 and max(x_rho.ravel())>180:
-            # check if grid cross lon=180 and change central lon for cartopy
-            mid=180
-            x_rho=x_rho-180
-            ox_psi=ox_psi-180
-        else:
-            mid=0
-
         self.figure.clf()
-
-        ax=self.figure.add_axes(rect=[0.1, 0.04, 0.80, 0.9],projection=ccrs.PlateCarree(central_longitude=mid))
-        ax.plot(ox_psi, oy_psi, 'r',zorder=5) #, ax=self.figure.gca(), zorder=3)
-
-        # Colorbar position
-        axpos = ax.get_position()
-        pos_x = axpos.x0 # + 0.25*axpos.width
-        pos_y = axpos.y0-0.08 #-axpos.height - 0.02
-        cax_width = axpos.width
-        cax_height = 0.04
-#        pos_cax = self.figure.add_axes([pos_x,pos_y,cax_width,cax_height])
-
         if smooth is None:
-            if 'grid points' in self.inputs.zview:
-                self.figure.suptitle('Grid points: rho=green, psi=blue')
-                ax.scatter(x_rho, y_rho, s=5, c='g', edgecolor='g', zorder=3)
-                ax.scatter(x_psi, y_psi, s=5, c='b', edgecolor='b', zorder=3)
-            elif 'topo' in self.inputs.zview:
-                cb= ax.pcolormesh(x_rho, y_rho, self.outputs.hraw, shading='auto',zorder=2)
-                pos_cax = self.figure.add_axes([pos_x,pos_y,cax_width,cax_height])
-                self.figure.colorbar(cb,cax=pos_cax,orientation='horizontal')
-                #M.scatter(x_rho, y_rho, s=2, c='w', edgecolor='w', ax=self.figure.gca(), zorder=3)
-
-            elif '1/pm' in self.inputs.zview:
-                cb = ax.pcolormesh(x_rho, y_rho, 1 / self.outputs.pm, zorder=2 )
-                #M.scatter(x_rho, y_rho, s=2, c='w', edgecolor='w', ax=self.figure.gca(), zorder=3)
-                pos_cax = self.figure.add_axes([pos_x,pos_y,cax_width,cax_height])
-                self.figure.colorbar(cb,cax=pos_cax,orientation='horizontal')
-
-            elif '1/pn' in self.inputs.zview:
-                cb = ax.pcolormesh(x_rho, y_rho, 1 / self.outputs.pn, zorder=2 )
-                #M.scatter(x_rho, y_rho, s=2, c='w', edgecolor='w', ax=self.figure.gca(), zorder=3)
-                pos_cax = self.figure.add_axes([pos_x,pos_y,cax_width,cax_height])
-                self.figure.colorbar(cb,cax=pos_cax,orientation='horizontal')
-
-            elif 'angle' in self.inputs.zview:
-                cb = ax.pcolormesh(x_rho, y_rho, self.outputs.angle, zorder=2 )
-                #M.scatter(x_rho, y_rho, s=2, c='w', edgecolor='w', ax=self.figure.gca(), zorder=3)
-                pos_cax = self.figure.add_axes([pos_x,pos_y,cax_width,cax_height])
-                self.figure.colorbar(cb,cax=pos_cax,orientation='horizontal')
-
-            elif 'mask' in self.inputs.zview:
-                cb = ax.pcolormesh(x_rho, y_rho, self.outputs.mask_rho, cmap=matplotlib.cm.Spectral,zorder=2 )
-            #M.scatter(x_rho, y_rho, s=2, c='k', edgecolor='k', ax=self.figure.gca(), zorder=3)
-
-            if not 'mask' in self.inputs.zview:
-                 path_shp=self.shp_file
-                 tch=shpreader.Reader(path_shp)
-                 # here ccrs define the projection of the data (coastline)
-                 shape_feature = cfeature.ShapelyFeature(tch.geometries(),ccrs.PlateCarree())
-
-                 ax.add_feature(shape_feature,facecolor='#929591',edgecolor='Gainsboro',zorder=4)
-                 gl=ax.gridlines(draw_labels=True, linewidth=2, color='gray', alpha=0.5, linestyle='--')
-                 gl.top_labels = False
-                 gl.right_labels = False
-
-            wx.CallAfter(self.figure.canvas.draw)
+            plot_grid(self.outputs, self.figure, self.inputs.zview, self.shp_file)
         else:
-            ax.pcolormesh(x_rho, y_rho,np.ma.masked_where(self.outputs.mask_rho>0, self.outputs.mask_rho) \
-                    ,vmin=0,vmax=1, zorder=2, cmap=matplotlib.pyplot.cm.copper_r)
-
-            cb = ax.pcolormesh(x_rho, y_rho, np.ma.masked_where(self.outputs.mask_rho<1, self.outputs.h) \
-                    , zorder=2 )
-            pos_cax = self.figure.add_axes([pos_x,pos_y,cax_width,cax_height])
-            self.figure.colorbar(cb,cax=pos_cax,orientation='horizontal')
-            gl=ax.gridlines(draw_labels=True, linewidth=2, color='gray', alpha=0.5, linestyle='--')
-            gl.top_labels = False
-            gl.right_labels = False
-
-            wx.CallAfter(self.figure.canvas.draw)
+            plot_topo(self.outputs, self.figure)
+        
+        wx.CallAfter(self.figure.canvas.draw)
 
     def grid_show_zm(self,prt_grd):
         def outline(lon, lat):
