59f1aaa0879b9a9940c94dc9cfe73be4df634d63
slgentil <sylvie.le.gentil@ifremer.fr>
2023-03-28 08:11:37 +0000

    fix bug backend AGG in batch

--- xcroco/plot.py
+++ xcroco/plot.py
@@ -11,14 +11,14 @@ import matplotlib.cm as cmx
 import cartopy.crs as ccrs
 
 # check if python run in batch mode or from jupyter (for plot in batch mode)
-import psutil
-def running_in_jupyter():
-    """Return True if any of our parent processes is jupyter"""
-    parent_names = [parent.name() for parent in psutil.Process().parents()]
-    return any('jupyter' in string for string in parent_names)
-if not running_in_jupyter():
-    import matplotlib
-    matplotlib.use('AGG')
+#import psutil
+#def running_in_jupyter():
+#    """Return True if any of our parent processes is jupyter"""
+#    parent_names = [parent.name() for parent in psutil.Process().parents()]
+#    return any('jupyter' in string for string in parent_names)
+#if not running_in_jupyter():
+#    import matplotlib
+#    matplotlib.use('AGG')
 
 import scipy.io
 from collections import OrderedDict
