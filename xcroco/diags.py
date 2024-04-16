e8d3c565b3b72ca9746979854a781c4e51200ca5
gula <gula@univ-brest.fr>
2024-04-16 12:07:10 +0200

    Remove xr.ufuncs, which is not deprecated as it can be replaced by numpy's ufuncs in all supported versions of numpy (https://docs.xarray.dev/en/stable/whats-new.html).

--- xcroco/diags.py
+++ xcroco/diags.py
@@ -248,7 +248,7 @@ def richardson(model, ds=None, u=None, v=None, rho=None, z=None, xgrid=None):
     dudz = xgrid.diff(u,'z') / xgrid.diff(gop.x2u(ds,z,xgrid),'z')
     dvdz = xgrid.diff(v,'z') / xgrid.diff(gop.x2v(ds,z,xgrid),'z')
 
-    Ri = xr.ufuncs.log10(N2 / (gop.x2w(ds,dudz,xgrid)**2 +  gop.x2w(ds,dvdz,xgrid)**2)).squeeze()
+    Ri = np.log10(N2 / (gop.x2w(ds,dudz,xgrid)**2 +  gop.x2w(ds,dvdz,xgrid)**2)).squeeze()
     if 'lon' in rho.coords: Ri = Ri.assign_coords(coords={"lon":rho.lon})
     if 'lat' in rho.coords: Ri = Ri.assign_coords(coords={"lat":rho.lat})
     Ri = Ri.assign_coords(coords={"z":z_w})
