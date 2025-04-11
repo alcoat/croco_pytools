6545440c1d24aebfa7b6576ffa6c4129cc61b1f3
Coat <alain.coat@actimar.fr>
2025-04-11 12:11:28 +0200

    extract rivers for med

--- prepro/download/glofas-forecast.py
+++ prepro/download/glofas-forecast.py
@@ -3,10 +3,19 @@
 import cdsapi
 import datetime
 import pathlib
+import xarray
 
 start_date = datetime.datetime(2025,4,6)
 # max lat, min lon, min lat, max lon
 area = [ 47.5, -10, 30, 42 ]
+rivers = {
+"rhone": (4.8499526,	43.3499475),
+"ebre":	(0.8999684,	40.7166247),
+"po":	(12.5499218,	44.9666077),
+"nil":	(30.3665172,	31.5166615),
+"marmara1":	(28.9831894,	41.0166235),
+"marmara2":	(28.999856,	41.0166235),
+}
 
 dataset = "cems-glofas-forecast"
 request = {
@@ -54,12 +63,24 @@ request = {
     "download_format": "unarchived"
 }
 
-client = cdsapi.Client(url = "https://ewds.climate.copernicus.eu/api", key = "778b31b7-7f4a-475b-a46b-476e50fc17a7")
-
 outpathname = pathlib.Path(f"../../../GLOFAS/glofas_{start_date:%Y%m%d}.nc")
-
 outpathname.parent.mkdir(parents=True, exist_ok=True)
 
+client = cdsapi.Client()
 client.retrieve(dataset, request).download(outpathname)
 
+ds = xarray.open_dataset(outpathname)
+ds = ds.squeeze("forecast_reference_time")
+ds = ds.drop_vars(["number", "surface","forecast_reference_time", "forecast_period"])
+ds = ds.rename({"forecast_period":"valid_time"})
+ds = ds.set_xindex(["valid_time"]).rename({"valid_time":"time", "dis24": "Qbar"})
+for name, pos in rivers.items():
+     ilon = ds.indexes["longitude"].get_indexer([pos[0]],method="nearest")[0]
+     ilat = ds.indexes["latitude"].get_indexer([pos[1]],method="nearest")[0]
+     print(name, ilon,ilat)
+     ds_data = ds.isel(longitude=slice(ilon-2,ilon+3), latitude=slice(ilat-2,ilat+3)).max(["longitude", "latitude"])
+     df_data = ds_data.to_dataframe()
+     outpname = pathlib.Path(f"../../../GLOFAS/{name}_{start_date:%Y%m%d}.csv")
+     outpname.parent.mkdir(parents=True, exist_ok=True)
+     df_data.to_csv(outpname)
 
