a6172524826a805a3abc77a0e2ca15d58a766e35
Coat <alain.coat@actimar.fr>
2025-04-17 11:26:27 +0200

    update downloads

--- prepro/download/copernicus.py
+++ prepro/download/copernicus.py
@@ -1,67 +1,146 @@
 #!/usr/bin/env python
 
+import argparse
 import datetime
 import pathlib
-import pandas
-import xarray
+import sys
+import logging
+
 import copernicusmarine
 import croco_class
+import pandas
+import xarray
 
-start_date = "2025-04-06 00:00"
-pd_start_date = pandas.Timestamp(start_date)
-pd_end_date = pd_start_date + datetime.timedelta(days=9,hours=18)
+logger = logging.getLogger(__name__)
 
-configname = "croco_med"
-grdpathname = "/home/shom_simuref/CROCO/fournitures_22AC05/MED_1.8/CROCO_FILES/test2.nc"
-#configname = "croco_gibr"
-#grdpathname = "../../croco_gibrtwo_inno_energy_grd.nc"
-destpath = "/home6/datawork/acoat/CROCO/MERCATOR"
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s %(levelname)s] [%(filename)s:%(lineno)s - %(funcName)s() ] %(message)s",
+)
 
-datasets = [
-   { "id": "cmems_mod_glo_phy-cur_anfc_0.083deg_PT6H-i", "vars": [ "uo", "vo" ] },
-   { "id": "cmems_mod_glo_phy-thetao_anfc_0.083deg_PT6H-i", "vars": [ "thetao" ] },
-   { "id": "cmems_mod_glo_phy-so_anfc_0.083deg_PT6H-i", "vars": [ "so" ] },
-   { "id": "cmems_mod_glo_phy_anfc_merged-sl_PT1H-i", "vars": [ "sea_surface_height" ] },
-   ]
+# configname = "croco_med"
+# grdpathname = "/home/shom_simuref/CROCO/fournitures_22AC05/MED_1.8/CROCO_FILES/test2.nc"
+# destpath = "/home6/datawork/acoat/CROCO/MERCATOR"
 
-grd = croco_class.CROCO_grd(grdpathname)
 
-buffer_zone = 0.5
+def download(start_date, end_date, work_dir, grdpathname, cfg):
+    datasets = [
+        {"id": "cmems_mod_glo_phy-cur_anfc_0.083deg_PT6H-i", "vars": ["uo", "vo"]},
+        {"id": "cmems_mod_glo_phy-thetao_anfc_0.083deg_PT6H-i", "vars": ["thetao"]},
+        {"id": "cmems_mod_glo_phy-so_anfc_0.083deg_PT6H-i", "vars": ["so"]},
+        {
+            "id": "cmems_mod_glo_phy_anfc_merged-sl_PT1H-i",
+            "vars": ["sea_surface_height"],
+        },
+    ]
 
-cm_request = {
-    "username" : "acoat",
-    "password" : "JAjyteva", 
-    "start_datetime": pd_start_date,
-    "end_datetime": pd_end_date,
-    "minimum_longitude": grd.lonmin() - buffer_zone,
-    "maximum_longitude": grd.lonmax() + buffer_zone,
-    "minimum_latitude": grd.latmin() - buffer_zone,
-    "maximum_latitude": grd.latmax() + buffer_zone,
-    "disable_progress_bar": True,
-    "no_metadata_cache": True,
-    "overwrite_metadata_cache": False,
-}
+    grd = croco_class.CROCO_grd(grdpathname)
 
-time_ax = None
-allds = {}
-for dataset in datasets:
-     req = cm_request.copy()
-     req["dataset_id"] = dataset["id"]
-     req["variables"] = dataset["vars"]
-     ds = copernicusmarine.open_dataset(**req)
-     if "sea_surface_height" in dataset["vars"]:
-         ds = ds.rename({"sea_surface_height": "zos"}).squeeze("depth")
-     else:
-         time_ax = ds.time
-     allds[dataset["id"]] = ds
+    buffer_zone = 0.5
 
-selds = []
-for did, ds in allds.items():
-    if time_ax is not None:
-        ds = ds.sel(time=time_ax)
-    encoding =  { var : { "zlib": True, "shuffle": True} for var in ds.data_vars }
-    outname = f"{did}_{pd_start_date:%Y%m%d%H}.nc"
-    outpath = pathlib.Path(destpath) / outname
-    outpath.parent.mkdir(parents=True, exist_ok=True)
-    ds.to_netcdf(outpath, encoding=encoding)
+    cm_request = {
+        "username": "acoat",
+        "password": "JAjyteva",
+        "start_datetime": start_date,
+        "end_datetime": end_date,
+        "minimum_longitude": grd.lonmin() - buffer_zone,
+        "maximum_longitude": grd.lonmax() + buffer_zone,
+        "minimum_latitude": grd.latmin() - buffer_zone,
+        "maximum_latitude": grd.latmax() + buffer_zone,
+        #"disable_progress_bar": True,
+        #"no_metadata_cache": True,
+        #"overwrite_metadata_cache": False,
+        "chunk_size_limit":0
+    }
 
+    time_ax = None
+    allds = {}
+    for dataset in datasets:
+        logger.info("Request dataset %s", dataset)
+        req = cm_request.copy()
+        req["dataset_id"] = dataset["id"]
+        req["variables"] = dataset["vars"]
+        ds = copernicusmarine.open_dataset(**req)
+        if "sea_surface_height" in dataset["vars"]:
+            ds = ds.rename({"sea_surface_height": "zos"}).squeeze("depth")
+        else:
+            time_ax = ds.time
+        allds[dataset["id"]] = ds
+    selds = []
+    for did, ds in allds.items():
+        logger.info("processing %s", did)
+        if (ds.time == time_ax).all() is not True:
+            if time_ax is not None:
+                ds = ds.sel(time=time_ax)
+        encoding = {var: {"zlib": True, "shuffle": True} for var in ds.data_vars}
+        outname = f"{cfg}_{did}_{start_date:%Y%m%dT%H%M}_{end_date:%Y%m%dT%H%M}.nc"
+        outpath = work_dir / outname
+        outpath.parent.mkdir(parents=True, exist_ok=True)
+        logger.info("Writing %s", outpath)
+        ds.to_netcdf(outpath, encoding=encoding)
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Copernicus Mercator Download",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    # add_logging_parser_arguments(parser)
+    parser.add_argument(
+        "--begindate", type=pandas.Timestamp, help="first date for the croco run"
+    )
+    parser.add_argument(
+        "--enddate", type=pandas.Timestamp, help="end date for the croco run"
+    )
+    parser.add_argument(
+        "--workdir",
+        type=pathlib.Path,
+        default=pathlib.Path("/home/opsys/data/CROCO/GTT_CROCO/MERCATOR"),
+        help="working directory path",
+    )
+    parser.add_argument(
+        "--cfgname",
+        type=str,
+        default="gibr",
+        help="configuration name",
+    )
+    parser.add_argument(
+        "--grd",
+        type=pathlib.Path,
+        default=pathlib.Path(
+            "/home/opsys/data/CROCO/GTT_CROCO/CROCO_FILES/croco_gibrtwo_inno_energy_grd.nc"
+        ),
+        help="grid pathname",
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+def run_main():
+    args = get_args()
+
+    if args.begindate is not None:
+        begindate = args.begindate
+    else:
+        begindate = pandas.Timestamp.utcnow().replace(
+            hour=0, minute=0, second=0, microsecond=0, nanosecond=0, tzinfo=None
+        ) - pandas.Timedelta(1, unit="day")
+    if args.enddate is not None:
+        enddate = args.enddate
+    else:
+        enddate = (
+            begindate
+            + pandas.Timedelta(5, unit="day")
+            #+ pandas.Timedelta(10, unit="day")
+            #- pandas.Timedelta(6, unit="hour")
+        )
+
+    download(begindate, enddate, args.workdir, args.grd, args.cfgname)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(run_main())
--- prepro/download/ecmwf.py
+++ prepro/download/ecmwf.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
+import logging
+import pathlib
+import sys
+import argparse
 
+import cfgrib
+import croco_class
 import herbie
+import metpy.calc
 import numpy
 import pandas
-import cfgrib
 import xarray
-import croco_class
-import pathlib
-import metpy.calc
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s %(levelname)s] [%(filename)s:%(lineno)s - %(funcName)s() ] %(message)s",
+)
+
 
 grd = croco_class.CROCO_grd("../../croco_gibrtwo_inno_energy_grd.nc")
 
@@ -16,58 +27,145 @@ maximum_longitude = grd.lonmax() + 0.5
 minimum_latitude = grd.latmin() - 0.5
 maximum_latitude = grd.latmax() + 0.5
 
-start_date=pandas.Timestamp("2025-03-17 00:00")
+start_date = pandas.Timestamp("2025-03-17 00:00")
 
-fxx = []
-fxx += list(range(0,144, 3))
-fxx += list(range(144, 240+6, 6))
 
-searchString='(?:10[uv]:|:2t:|:2d:|:msl:|:ssr:|:str:|:tp:)'
+def download(start_date, end_date, work_dir, grdpathname, cfg):
 
-outdir = pathlib.Path("../../../ECMWF")
+    grd = croco_class.CROCO_grd(grdpathname)
 
-while True:
-    FH = herbie.FastHerbie([start_date], model="ecmwf", product="oper", fxx=fxx, save_dir=outdir)
+    minimum_longitude = grd.lonmin() - 0.5
+    maximum_longitude = grd.lonmax() + 0.5
+    minimum_latitude = grd.latmin() - 0.5
+    maximum_latitude = grd.latmax() + 0.5
 
-    files = FH.download(searchString)
+    fxx = []
+    fxx += list(range(0, 144, 3))
+    fxx += list(range(144, 240 + 6, 6))
 
-    allok = True
-    allds = []
-    for fpath in files:
-        ds = cfgrib.open_datasets(fpath)
-        ds = xarray.merge(ds, compat="override").sortby('latitude')
-        if len(ds.data_vars)<8:
-            print("bad file", fpath, len(ds.data_vars))
-            print(ds)
-            fpath.unlink()
-            alltok = False
-            continue
-        ds = ds.sel(latitude=slice(minimum_latitude, maximum_latitude), longitude=slice(minimum_longitude,maximum_longitude))
-        allds.append(ds)
+    searchString = "(?:10[uv]:|:2t:|:2d:|:msl:|:ssr:|:str:|:tp:)"
 
-    if allok is True and len(allds)>0:
-        break
+    while True:
+        FH = herbie.FastHerbie(
+            [start_date], model="ecmwf", product="oper", fxx=fxx, save_dir=work_dir
+        )
 
-ds = xarray.concat(allds, dim="valid_time")
-ds = ds.drop('time').rename({'valid_time': 'time'})
-ds.time.encoding['units'] = "days since 1900-01-01T00:00:00Z"
+        files = FH.download(searchString)
 
-# RH: =100*(EXP((17.625*TD)/(243.04+TD))/EXP((17.625*T)/(243.04+T)))
-# q :  equation 4.24, Pg 96 Practical Meteorolgy (Roland Stull) https://github.com/Unidata/MetPy/issues/791
-#ds["rh2m"] = numpy.exp(17.625*ds["d2m"] / (243.04 + ds["d2m"])) / numpy.exp(17.625*ds["t2m"]/(243.04+ds["t2m"]))
-#ds["rh2m"] = xarray.where(ds['rh2m']<1, ds['rh2m'], 1)
-ds["hu2m"] = metpy.calc.relative_humidity_from_dewpoint(ds["t2m"], ds["d2m"])
-#ds["hu2m"] = xarray.where(ds['hu2m']<1, ds['hu2m'], 1)
-ds = ds.drop("d2m")
-ds['ssr'] = ds['ssr'].differentiate("time", datetime_unit="s")
-ds['ssr'] = xarray.where(ds['ssr']>0, ds['ssr'], 0)
-ds['str'] = - ds['str'].differentiate("time", datetime_unit="s")
-ds['tp'] = ds['tp'].differentiate("time", datetime_unit="s")
+        allok = True
+        allds = []
+        for fpath in files:
+            ds = cfgrib.open_datasets(fpath)
+            ds = xarray.merge(ds, compat="override").sortby("latitude")
+            if len(ds.data_vars) < 8:
+                print("bad file", fpath, len(ds.data_vars))
+                print(ds)
+                fpath.unlink()
+                alltok = False
+                continue
+            ds = ds.sel(
+                latitude=slice(minimum_latitude, maximum_latitude),
+                longitude=slice(minimum_longitude, maximum_longitude),
+            )
+            allds.append(ds)
 
-ds = ds.rename({"u10": "u10m", "v10": "v10m", "msl": "pmer", "ssr": "flsolaire", "str": "fltherm", "tp": "prate"})
-ds = ds.rename({"latitude": "lat", "longitude": "lon" })
-encoding =  { var : { "zlib": True, "shuffle": True} for var in ds.data_vars }
-outname = f"ecmwf_{start_date:%Y%m%d%H}.nc"
-outpath = outdir / outname
-ds.to_netcdf(outpath, encoding=encoding)
+        if allok is True and len(allds) > 0:
+            break
 
+    ds = xarray.concat(allds, dim="valid_time")
+    ds = ds.drop("time").rename({"valid_time": "time"})
+    ds.time.encoding["units"] = "days since 1900-01-01T00:00:00Z"
+
+    # RH: =100*(EXP((17.625*TD)/(243.04+TD))/EXP((17.625*T)/(243.04+T)))
+    # q :  equation 4.24, Pg 96 Practical Meteorolgy (Roland Stull) https://github.com/Unidata/MetPy/issues/791
+    # ds["rh2m"] = numpy.exp(17.625*ds["d2m"] / (243.04 + ds["d2m"])) / numpy.exp(17.625*ds["t2m"]/(243.04+ds["t2m"]))
+    # ds["rh2m"] = xarray.where(ds['rh2m']<1, ds['rh2m'], 1)
+    ds["hu2m"] = metpy.calc.relative_humidity_from_dewpoint(ds["t2m"], ds["d2m"])
+    # ds["hu2m"] = xarray.where(ds['hu2m']<1, ds['hu2m'], 1)
+    ds = ds.drop("d2m")
+    ds["ssr"] = ds["ssr"].differentiate("time", datetime_unit="s")
+    ds["ssr"] = xarray.where(ds["ssr"] > 0, ds["ssr"], 0)
+    ds["str"] = -ds["str"].differentiate("time", datetime_unit="s")
+    ds["tp"] = ds["tp"].differentiate("time", datetime_unit="s")
+
+    ds = ds.rename(
+        {
+            "u10": "u10m",
+            "v10": "v10m",
+            "msl": "pmer",
+            "ssr": "flsolaire",
+            "str": "fltherm",
+            "tp": "prate",
+        }
+    )
+    ds = ds.rename({"latitude": "lat", "longitude": "lon"})
+    encoding = {var: {"zlib": True, "shuffle": True} for var in ds.data_vars}
+    outname = f"{cfg}_ecmwf_{start_date:%Y%m%d%H}.nc"
+    outpath = work_dir / outname
+    ds.to_netcdf(outpath, encoding=encoding)
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Copernicus Mercator Download",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    # add_logging_parser_arguments(parser)
+    parser.add_argument(
+        "--begindate", type=pandas.Timestamp, help="first date for the croco run"
+    )
+    parser.add_argument(
+        "--enddate", type=pandas.Timestamp, help="end date for the croco run"
+    )
+    parser.add_argument(
+        "--workdir",
+        type=pathlib.Path,
+        default=pathlib.Path("/home/opsys/data/CROCO/GTT_CROCO/ECMWF"),
+        help="working directory path",
+    )
+    parser.add_argument(
+        "--cfgname",
+        type=str,
+        default="gibr",
+        help="configuration name",
+    )
+    parser.add_argument(
+        "--grd",
+        type=pathlib.Path,
+        default=pathlib.Path(
+            "/home/opsys/data/CROCO/GTT_CROCO/CROCO_FILES/croco_gibrtwo_inno_energy_grd.nc"
+        ),
+        help="grid pathname",
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+def run_main():
+    args = get_args()
+
+    if args.begindate is not None:
+        begindate = args.begindate
+    else:
+        begindate = pandas.Timestamp.utcnow().replace(
+            hour=0, minute=0, second=0, microsecond=0, nanosecond=0, tzinfo=None
+        ) - pandas.Timedelta(1, unit="day")
+    if args.enddate is not None:
+        enddate = args.enddate
+    else:
+        enddate = (
+            begindate
+            + pandas.Timedelta(5, unit="day")
+            # + pandas.Timedelta(10, unit="day")
+            # - pandas.Timedelta(6, unit="hour")
+        )
+
+    download(begindate, enddate, args.workdir, args.grd, args.cfgname)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(run_main())
